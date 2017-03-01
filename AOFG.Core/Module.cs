ae586c411ba485fb3d4b675db2c9f95872dcd83a
Aler Denisov <aler.zampillo@gmail.com>
2017-03-02 00:55:36 +0300

    Base directory structure

---
+++ AOFG.Core/Module.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace AOFG.Core
+{
+    /// <summary>
+    /// Core modules wrapper
+    /// </summary>
+    public class Module
+    {
+        /// <summary>
+        /// Initials module instance.
+        /// </summary>
+        public void Initial() { }
+
+        /// <summary>
+        /// Fabric method to create instance of module
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="System.NotSupportedException">Not implemented yet</exception>
+        public static Module Create()
+        {
+            throw new NotSupportedException("Not implemented yet");
+        }
+    }
+}
---
+++ AOFG.Core/Types/Serial.cs
@@ -0,0 +1,16 @@
+﻿namespace AOFG.Core.Types
+{
+    /// <summary>
+    /// Serial number data type
+    /// <remarks>
+    /// Serial type is core identifier between client and server
+    /// </remarks>
+    /// </summary>
+    public struct Serial
+    {
+        /// <summary>
+        /// Integer representation of serial
+        /// </summary>
+        public int Value;
+    }
+}
